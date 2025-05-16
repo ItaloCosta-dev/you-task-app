bdcdbb8e8b7f42f00e0af386fe72f9c79eeff8ac
ItaloCosta-dev <italocosta.dev@gmail.com>
2025-05-16 09:19:12 -0300

    feat: Kinde Auth Integration.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
+import { Button } from "@/components/ui/button"
+import {  
+  RegisterLink,
+  LoginLink
+} from "@kinde-oss/kinde-auth-nextjs/components"
 
-
-export default function Home() {
+const page = () => {
   return (
-   <div >
-    
-   </div>
-  );
+    <div className="w-full h-screen flex flex-col items-center justify-center">
+      <h1 className="text-5xl font-bold text-center">Bem vindo ao You Task</h1>
+
+      <div className="flex gap-4 mt-6">
+        <Button>
+          <RegisterLink>Começar agora!</RegisterLink>
+        </Button>
+
+        <Button asChild variant={"outline"}>
+          <LoginLink>Entrar</LoginLink>
+        </Button>
+      </div>
+    </div>
+  )
 }
+
+export default page
